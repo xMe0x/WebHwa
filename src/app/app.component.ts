4400b3b4d9c3be9a5f0605fb4b4ecb7f2c90e24d
FIRST_NAME LAST_NAME <markzanazer4@gmail.com>
2025-03-09 19:28:51 +0700

    update agin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { DetailComponent } from './detail/detail.component';
+import { CatagoryComponent } from "./catagory/catagory.component";
 
 @Component({
   selector: 'app-root',
-  imports: [DetailComponent],
+  imports: [CatagoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
